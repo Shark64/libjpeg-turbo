9a833995fe3b2bfa2a63b650d6fcbcd1e438e2d6
Shark <shark@krellmachine>
2018-10-25 17:58:22 +0200

    CPUID uses 43bit registers only, no need for REX prefixes. Switch short branches to cmov

--- simd/x86_64/jsimdcpu.asm
+++ simd/x86_64/jsimdcpu.asm
@@ -36,39 +36,38 @@ EXTN(jpeg_simd_cpu_support):
     push        rbx
     push        rdi
 
-    xor         rdi, rdi                ; simd support flag
 
     ; Check for AVX2 instruction support
-    mov         rax, 7
-    xor         rcx, rcx
+    xor         ecx, ecx
+    lea         edi, [rcx+(JSIMD_SSE2|JSIMD_SSE)]
+    lea         eax, [rcx+7]
     cpuid
-    mov         rax, rbx                ; rax = Extended feature flags
 
-    or          rdi, JSIMD_SSE2
-    or          rdi, JSIMD_SSE
-    test        rax, 1<<5               ; bit5:AVX2
+    test        bl, 1<<5               ; bit5:AVX2
     jz          short .return
 
     ; Check for AVX2 O/S support
-    mov         rax, 1
-    xor         rcx, rcx
+    xor         ecx, ecx
+    lea         eax, [rcx+1]
     cpuid
-    test        rcx, 1<<27
-    jz          short .return           ; O/S does not support XSAVE
-    test        rcx, 1<<28
-    jz          short .return           ; CPU does not support AVX2
-
-    xor         rcx, rcx
+    xor		ebx, ebx
+    shr		ecx, 27
+    and		ecx, 3
+    cmp		 cl, 3
+    sete         bl                      ; O/S does support XSAVE+AVX2
+    
+    xor         ecx, ecx
     xgetbv
-    and         rax, 6
-    cmp         rax, 6                  ; O/S does not manage XMM/YMM state
-                                        ; using XSAVE
-    jnz         short .return
-
-    or          rdi, JSIMD_AVX2
-
+    xor		edx, edx
+    mov		ecx, edi
+    or		ecx, JSIMD_AVX2
+    and         eax, 6
+    cmp          al, 6                  ; O/S manage XMM/YMM state with XSAVE
+    sete	 dl
+    test	 bl, dl
+    cmovnz	edi, ecx 
 .return:
-    mov         rax, rdi
+    mov         eax, edi
 
     pop         rdi
     pop         rbx
